d815709421bfb21bc865ca5144e49c5f571f4862
Franco Alemandi <francolp59@gmail.com>
2025-06-25 17:02:53 -0300

    feat: add GameSearch Storybook story
    - Create placeholder story structure for GameSearch component
    - Maintain consistency with existing stories format
    - Enable component documentation and testing in Storybook

---
+++ stories/GameSearch.stories.ts
@@ -0,0 +1,7 @@
+// Simple story for GameSearch component
+export default {
+  title: "Molecules/GameSearch",
+  component: null, // Will be set dynamically
+};
+
+export const Default = () => null; // Placeholder story
